6ec8d6a511b370cc4b2453669159dff68c431e22
christoforadiana <christoforadianayuliawati@gmail.com>
2022-01-29 22:58:15 +0700

    (LKP) ListView & Library

---
+++ MyListView/app/src/main/java/com/example/mylistview/Hero.java
@@ -0,0 +1,31 @@
+package com.example.mylistview;
+
+public class Hero {
+    private int photo;
+    private String name;
+    private String desription;
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesription() {
+        return desription;
+    }
+
+    public void setDesription(String desription) {
+        this.desription = desription;
+    }
+}
